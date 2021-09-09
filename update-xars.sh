e92c15ddce75667968caaa5ae7ec0111aa070f07
Benjamin W. Bohl <b.w.bohl@gmail.com>
2021-09-09 14:11:33 +0200

    Update update-xars.sh
    * Add documentation
    * Add help

--- update-xars.sh
+++ update-xars.sh
@@ -5,12 +5,6 @@ echo "#################################"
 echo "# Welcome to the update-xars.sh #"
 echo "#################################"
 echo
-echo "This script will fetch the latest versions of XARs for eXist-db."
-echo "The target version of eXist-db has to be submitted as first argument."
-echo "The URL of the repo from which to fetch the XARs can be submitted as"
-echo "second parameter but will default to the eXist-db public repo at:"
-echo "https://exist-db.org/exist/apps/public-repo/public"
-echo
 # check submitted arguments
 echo "checking submitted arguments…"
 echo
@@ -20,9 +14,71 @@ then
     echo "You have to submit at least one argument indicating"
     echo "the target eXist-db version. For more information see:"
     echo "https://github.com/peterstadler/existdb-docker"
+    echo "or execute with first argument being:"
+    echo "help"
     echo
     exit
 fi
+
+if [[ "$1" == "help" ]]
+then
+    echo "You called help for using this script…"
+    echo
+    echo "########"
+    echo "# HELP #"
+    echo "########"
+    echo
+    echo 
+    echo "This script will fetch the latest versions of XARs for eXist-db."
+    echo "The target version of eXist-db has to be submitted as first argument."
+    echo "The URL of the repo from which to fetch the XARs can be submitted as"
+    echo "second argument but will default to the eXist-db public repo at:"
+    echo "https://exist-db.org/exist/apps/public-repo/public"
+    echo
+    echo "A folder location can be submitted as third argument. The downloaded"
+    echo "XARs will be copied to this location after completing all downloads."
+    echo "Please be aware that you will have to submit the repo URL as second"
+    echo "argument if you want to specify a target folder!"
+    echo
+    echo "EXAMPLE USAGE"
+    echo "============="
+    echo
+    echo "1. \$ ./update-xars.sh"
+    echo "-------------------"
+    echo
+    echo "Will raise an error and prompt an error message."
+    echo
+    echo
+    echo "2. \$ ./update-xars.sh 5.2.0"
+    echo "-------------------------"
+    echo
+    echo "Will download the latest XAR versions for eXist-db version 5.2.0."
+    echo "The XARs will be downloaded to a system temporary directory."
+    echo "The location of the temporary directory will be prompted at the end of the script."
+    echo
+    echo
+    echo "3. \$ ./update-xars.sh 5.2.0 https://your-custom-repo-url/public"
+    echo "----------------------------------------------------------------"
+    echo
+    echo "Same as 2. but the XARs will be fetched from https://your-custom-repo-url/public."
+    echo
+    echo
+    echo "4. \$ ./update-xars.sh 5.2.0 https://your-custom-repo-url/public ./your/custom/target/directory"
+    echo "-----------------------------------------------------------------------------------------------"
+    echo
+    echo "Same as 3. but the downloaded XARs will be copied to ./your/custom/target/directory"
+    echo "after downloading has finished."
+    echo
+    echo "If ./your/custom/target/directory does not already exist, it will be created by this script."
+    echo
+    echo "Please be aware that ./your/custom/target/directory may already contain"
+    echo "other files. Theses could interfere when using the directory as autodeploy"
+    echo "directory with eXist-db. If you want to prune the directory's contents before"
+    echo "copying the newly downloaded XARs, please submit 'prune' as fourth argument."
+    echo ""
+    exit
+fi
+
 # $VERSION is the eXist-db version
 VERSION=$1
 # $XAR_REPO_URL is the URL to the XAR repo where the XAR packages will be fetched
